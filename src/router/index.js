eb95b072f2071b1af32569cd6a47ca031ddf75fd
Bastian Lobos <blobosarevalo@gmail.com>
2025-07-22 16:04:55 -0400

    Update index.js
    S

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
       { path: 'registro', component: () => import('pages/RegistroPage.vue') },
       { path: 'cuidador', component: () => import('pages/CuidadorPage.vue') },
       { path: 'paciente', component: () => import('pages/PacientePage.vue') },
-      { path: 'admin', component: () => import('pages/AdminDashboardPage.vue') }, // ⬆️ AQUÍ AGREGADO
+      { path: 'admin', component: () => import('src/pages/AdminPage.vue') }, // ⬆️ AQUÍ AGREGADO
     ],
   },
   {
