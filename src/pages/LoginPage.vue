e16741ba1412be67f1bd1167add04b219bd57041
Bastian Lobos <blobosarevalo@gmail.com>
2025-07-24 16:09:35 -0400

    Update LoginPage.vue

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -76,7 +76,7 @@ const login = async () => {
     setTimeout(() => {
       cargando.value = false
       router.push(destino)
-    }, 6000)
+    }, 3000)
   } catch (err) {
     console.error('❌ Error al conectar al backend:', err)
     if (err.response) {
