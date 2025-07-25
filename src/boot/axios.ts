b732def8fdb344241ae8e24e42e6cbc984e3eb49
Bastian Lobos <blobosarevalo@gmail.com>
2025-07-25 13:49:13 -0400

    d
    d

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -2,7 +2,7 @@ import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:3000',
+  baseURL: process.env.API_URL || 'https://medialert-backend-1q8e.onrender.com',
 })
 
 export default boot(({ app }) => {
